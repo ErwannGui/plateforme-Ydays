83dae931a194839b8fc50982ba34d8315313ebfd
ErwannGui <erwann.guillevic@ynov.com>
2017-12-08 01:35:54 +0100

    Petit test perso
    J'envoie des données à ma vue !!
    Et j'ai testé ma connection a ma bdd et je peux recuperer des donnes !

--- plateforme_ydays/src/AppBundle/Controller/DefaultController.php
+++ plateforme_ydays/src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,6 +14,16 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        // Récupération de la session
+        $session = $request->getSession();
+        
+        // On récupère le contenu de la variable user_id
+        $userId = $session->get('user_id');
+
+        // On définit une nouvelle valeur pour cette variable user_id
+        $session->set('user_id', 91);
+
+        return new Response("Je suis une page de test, je n'ai rien à dire".$userId);
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
