984dbe2e187351b384e4af97d8e3ea68c5fcdf0b
ErwannGui <erwann.guillevic@ynov.com>
2017-12-12 19:48:47 +0100

    re-ajout entity et repo

---
+++ plateforme_ydays/src/AppBundle/Entity/Salle.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ @author Groupe 5 <https://github.com/ErwannGui/plateforme-Ydays>
+ @copyright 2017 Groupe 5
+ @version 1.0
+*/
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * salle
+ *
+ * @ORM\Table(name="salle")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\salleRepository")
+ */
+class Salle
+{
+    //génération de la classe Salle et de ses guetters/setters ce qui permet de créer des objets salles et de les utiliser dans notre application
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="numero", type="integer")
+     */
+    private $numero;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return salle
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set numero
+     *
+     * @param integer $numero
+     *
+     * @return salle
+     */
+    public function setNumero($numero)
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    /**
+     * Get numero
+     *
+     * @return int
+     */
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+}
+
